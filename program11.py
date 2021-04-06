8a455e4cf15b245cde8a255ba564aff0ef11d38f
Harkeerat Singh <63100973+harkred@users.noreply.github.com>
2021-04-06 18:14:10 +0530

    Update program11.py

--- program11.py
+++ program11.py
@@ -1,6 +1,9 @@
+#To print out longest wordfrom a list
 def longest_word(lst):
     length = [len(i) for i in lst]
     return 'Longest word is {}'.format(lst[length.index(max(length))])
 
-lst = ['Amazon', 'Facebook', 'Apple', 'Microsoft']
-print(longest_word(lst))
+#__main__
+if __name__ == '__main__':
+    lst = ['Amazon', 'Facebook', 'Apple', 'Microsoft']
+    print(longest_word(lst))
