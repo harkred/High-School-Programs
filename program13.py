a0af23a9b09fe24e68f87b56817959244c14e2bb
Harkeerat Singh <63100973+harkred@users.noreply.github.com>
2021-04-06 18:16:30 +0530

    Update program13.py

--- program13.py
+++ program13.py
@@ -1,9 +1,12 @@
-def _counter(dictionary, lst):
+#Recreating counter functions from collections module
+def _counter(lst):
+    dictionary = {}
     new = set(lst)
     for i in new:
         dictionary[i] = lst.count(i)
     return dictionary
 
-dictionary = {}
-lst =[1,2,3,1,2,3,4,2,12,45,2]
-print(_counter(dictionary, lst))
+#__main__
+if __name__ == '__main__':
+    lst =[1,2,3,1,2,3,4,2,12,45,2]
+    print(_counter(lst))
